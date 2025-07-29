496c975985710eb2579af74bc14f303ba621db52
‪Yaniv Weinshtein‬‏ <yanivwein22@gmail.com>
2025-07-29 09:47:46 +0300

    add a temp products object for POC testing

---
+++ src/data/products.ts
@@ -0,0 +1,40 @@
+import Product from "types/Product"
+import Pinuk_Pink from "../assets/images/Pinuk_Pink.png"
+import Pinuk_Blue from "../assets/images/Pinuk_Blue.png"
+import Sano_Oxygen from "../assets/images/Sano_Oxygen.webp"
+
+
+    const products: Product[] = [
+    {
+    "id": 0,
+    "name": "פינוק מרכך לשיער יבש/פגום",
+    "label": "7290112492548_306",
+    "price": 10,
+    "discountPrice": 8,
+    "memberPrice": 7,
+    "description": "מרכך פינוק מסייע בהזנת השיער בלחות למראה בריא ומלא חיוניות. הנוסחה העשירה מטפחת את שיערך ומעניקה לו רכות וגמישות לעיצוב וסירוק קל לאחר החפיפה.",
+    "image": Pinuk_Blue
+    },
+    {
+    "id": 1,
+    "name": "שמפו פינוק לשיער רגיל",
+    "label": "7290112492531_206",
+    "price": 20,
+    "discountPrice": 18,
+    "memberPrice": 17,
+    "description": "שמפו פינוק מסייע בהזנת השיער ועור הקרקפת בלחות ומעניק לשיערך רכות וגמישות. הנוסחה העשירה מטפחת את שיערך ומותירה אותו במראה בריא ומלא חיוניות.",
+    "image": Pinuk_Pink
+    },
+    {
+    "id": 2,
+    "name": "סנו אוקסיג׳ן",
+    "label": "7290000288024_301",
+    "price": 30,
+    "discountPrice": 28,
+    "memberPrice": 27,
+    "description": "סנו, נוסחא חדשה, מרסס אוקסיג'ן עם חמצן פעיל, ללא אקונומיקה, מסיר כתמים משפר תוצאות של כביסה צבעונית ולבנה,אנטי בקטריאלי קוטל 99.9% מהחיידקים. לטיפול בכתמים, חפתים וצווארונים לפני הכביסה. עם חמצן פעיל Oxygen ללא אקונומיקה: שומר על צבע",
+    "image": Sano_Oxygen
+    },
+    ]
+
+    export default products
